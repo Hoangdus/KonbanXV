b80aa868bf224e51a48f128132d200b4642091c8
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2024-03-15 02:00:44 +0700

    Update preference panel

--- Prefs/Preferences.h
+++ Prefs/Preferences.h
@@ -2,7 +2,6 @@
 #import <Preferences/PSListController.h>
 #import <Preferences/PSSpecifier.h>
 #import <CepheiPrefs/HBRootListController.h>
-// #import <CepheiPrefs/HBAppearanceSettings.h>
 #import <Cephei/HBPreferences.h>
 #import "NSTask.h"
 
